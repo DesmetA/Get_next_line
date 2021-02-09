01e19c5aca98fc35b6821f0a258b4f2a6b71c7dc
desmeta <desmeta@student.s19.be>
2021-02-09 10:02:11 +0100

    First commit

---
+++ get_next_line_bonus.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adesmet <adesmet@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/08 14:31:15 by adesmet           #+#    #+#             */
+/*   Updated: 2021/02/08 15:12:05 by adesmet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <fcntl.h>
+# include <limits.h>
+# include <stdio.h>
+
+int ft_newline(char *str);
+int ft_get_line(char *stack, char **line, int nl);
+char	*ft_join(char const *s1, char const *s2);
+int get_next_line(int fd, char **line);
+
+size_t	ft_strlen(const char *str);
+void	*ft_calloc(size_t nmemb, size_t size);
+void	*ft_memmove(void *dest, const void *src, size_t len);
+char	*ft_strdup(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adesmet <adesmet@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/07 21:01:47 by adesmet           #+#    #+#             */
+/*   Updated: 2021/02/08 14:43:28 by adesmet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	int i;
+
+	i = -1;
+	while (str[++i])
+		;
+	return (i);
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	unsigned char	*ptr;
+	size_t	i;
+
+	i = -1;
+	if (!(ptr = malloc(sizeof(char) * (size * nmemb))))
+		return (NULL);
+	while(++i<size*nmemb)
+		ptr[i]='\0';
+	return (ptr);
+}
+
+void	*ft_memmove(void *dest, const void *src, size_t len)
+{
+	unsigned char	*ptr;
+	unsigned char	*ptr2;
+	size_t			i;
+
+	if (!dest && !src)
+		return (NULL);
+	ptr = (unsigned char *)dest;
+	ptr2 = (unsigned char *)src;
+	i = -1;
+	if (ptr < ptr2)
+	{
+		while (++i < len)
+			ptr[i] = ptr2[i];
+	}
+	if (ptr > ptr2)
+	{
+		while (len--)
+			ptr[len] = ptr2[len];
+	}
+	return (dest);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*s2;
+	size_t	i;
+
+	i = -1;
+	if (!(s2 = (char *)(ft_calloc(sizeof(char), ft_strlen(s) + 1))))
+		return (NULL);
+	while (s[++i])
+		s2[i] = s[i];
+	return (s2);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*ptr;
+	size_t	i;
+	size_t	len2;
+
+	len2 = len;
+	if (!s)
+		return (NULL);
+	if (ft_strlen(s) - start + 1 < len)
+		len2 = ft_strlen(s) - start;
+	if (ft_strlen(s) < start)
+	{
+		if (!(ptr = (char *)ft_calloc(sizeof(char), 1)))
+			return (NULL);
+		return (ptr);
+	}
+	if (!(ptr = ft_calloc(sizeof(char), len2 + 1)))
+		return (NULL);
+	i = -1;
+	while (++i < len2)
+		ptr[i] = s[start + i];
+	return (ptr);
+}
